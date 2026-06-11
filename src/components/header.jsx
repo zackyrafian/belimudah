ad6916a21688a9b57a5a8b925731afb49e08d355
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-06-12 00:11:53 +0700

    feat: add header component

---
+++ src/components/header.jsx
@@ -0,0 +1,49 @@
+import { Bell, User, Heart, ShoppingCart , MapPin } from 'lucide-react';
+
+function Header() {
+  return (
+    <div className="w-full border-b border-b-black/20 shadow">
+      <div className="bg-blue-500 flex items-center w-full">
+        <div className="w-7xl mx-auto text-white py-1 flex items-center gap-2">
+          <MapPin size={11} />
+          <span>Kirim ke: Jakarta Selatan</span>
+        </div>
+      </div>
+
+      <div className="w-full p-2 border-b border-black/20">
+        <div className="max-w-7xl mx-auto py-1 flex gap-4 items-center justify-between">
+            <div className="flex items-center gap-2">
+              <div className="bg-blue-500 w-8 h-8 flex items-center justify-center rounded-md p-2 text-white">B</div>
+              <span>BeliMudah</span>
+            </div>
+            <div className="w-full border border-black/20 bg-black/5 rounded-xl flex items-center">
+              <input className='w-full rounded-xl p-2' type="text" placeholder='Cari Produk, merek, kategori...' />
+            </div>
+            <div className='flex gap-4'>
+              <Bell size={20}/>
+              <User size={20} />
+              <Heart size={20}/>
+              <ShoppingCart size={20}/>
+            </div>
+        </div>
+      </div>
+
+      <div className='w-full'>
+        <div className='w-7xl mx-auto py-1 flex items-center justify-center gap-12'>
+          <div>
+            <span>Semua Category</span>
+          </div>
+          <span>Elektornik</span>
+          <span>Fashion</span>
+          <span>Rumah & Dapur</span>
+          <span>Kecantikan</span>
+          <span>Olahraga</span>
+          <span>Buku & Alamat Tulis</span>
+          <span>Promo</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export { Header };
