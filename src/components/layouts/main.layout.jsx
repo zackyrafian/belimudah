d24ec2faea45a80f2edc718b7aadc2b5a0ca4390
Zacky Rafian <imftcy@gmail.com>
2026-06-13 16:53:10 +0700

    feat: add mainLayout

---
+++ src/components/layouts/main.layout.jsx
@@ -0,0 +1,14 @@
+import { Footer } from "../footer";
+import { Header } from "../header";
+
+export default function MainLayout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="max-w-7xl w-full mx-auto flex-1 py-8">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
