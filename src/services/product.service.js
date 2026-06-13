2968eef100b9ab5827c2728c45b18b139de17353
Zacky Rafian <imftcy@gmail.com>
2026-06-14 01:34:19 +0700

    feat: add get product by name

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -3,6 +3,9 @@ import products from '@/data/products.json';
 const ProductService = {
   getAll() {
     return products;
+  }, 
+  getByName(name) { 
+    return products.find(product => product.name.toLowerCase().replaceAll(" ", "-") === name)
   }
 }
 
