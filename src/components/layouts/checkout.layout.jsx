98c4116a78e17d01498174ec7ef121ecc17cb23b
Zacky Rafian <imftcy@gmail.com>
2026-06-13 20:46:23 +0700

    feat: add checkout layout

---
+++ src/components/layouts/checkout.layout.jsx
@@ -0,0 +1,46 @@
+import { Outlet } from "react-router";
+import Card from "../card";
+import MainLayout from "./main.layout";
+
+export default function CheckoutLayout ()  { 
+  return (
+    <MainLayout>
+      <div className="flex gap-4" >
+        <Card className="flex-1">
+          <Outlet/>
+        </Card>
+        <Card className="w-1/4 flex-col gap-2 flex">
+          <div>Ringkasan Pesan</div>
+          <div className="flex justify-between items-center">
+            <div className="flex gap-2 items-center">
+              <div className="w-10 h-10 rounded-lg overflow-hidden">
+                <img src="/headphone.png" alt="" />
+              </div>
+              <span className="text-xs">Headphone Wireless Premium</span>
+            </div>
+            <div>
+              <span>x1</span>
+            </div>
+          </div>
+          <div className="flex flex-col gap-1">
+            <div className="flex justify-between">
+              <span>Subtotal</span>
+              <span>Rp 450.000</span>
+            </div>
+            <div className="flex justify-between border-b border-b-black/20">
+              <span>Ongkir</span>
+              <span>Gratis</span>
+            </div>
+            <div className="flex justify-between pt-1">
+              <span>Total</span>
+              <span>Rp 450.000</span>
+            </div>
+          </div>
+          <div className="text-center text-xs">
+            <span>Pembayaran aman dan terenkripsi</span>
+          </div>
+        </Card>
+      </div>
+    </MainLayout> 
+  )
+}
