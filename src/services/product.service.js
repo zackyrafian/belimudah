00f21360f2ba90e5ab781a3885792704429b3045
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-06-14 13:44:57 +0700

    feat: add getcategories service

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -6,6 +6,21 @@ const ProductService = {
   }, 
   getByName(name) { 
     return products.find(product => product.name.toLowerCase().replaceAll(" ", "-") === name)
+  }, 
+  getCategories(limit) { 
+    const categories = {};
+
+    products.forEach(product => { 
+      if (!categories[product.category]) { 
+        categories[product.category] = { 
+          name: product.category, 
+          total: 0, 
+        }
+      }
+      categories[product.category].total++; 
+    })
+    const result = Object.values(categories); 
+    return limit ? result.slice(0, limit) : result;
   }
 }
 
