8fbbfce3cfa3f6c7ce2cf4a456f9d9f474f96791
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-06-15 22:29:59 +0700

    style: add border & shadow card

--- src/components/layouts/profile.layout.jsx
+++ src/components/layouts/profile.layout.jsx
@@ -36,13 +36,13 @@ export default function ProfileLayout() {
     <MainLayout>
       <div className="flex flex-row gap-8">
         <div className="w-1/5 flex flex-col gap-4">
-          <div className="flex flex-col gap-2 rounded-xl items-center justify-center bg-white border-black/20 p-4">
+          <div className="flex flex-col gap-2 rounded-xl shadow-sm items-center justify-center bg-white border-black/20 border p-4">
             <div className="rounded-full w-16 h-16 bg-blue-500 flex justify-center items-center">B</div>
             <div>Budi Santoso</div>
             <div>budi@email.com</div>
           </div>
 
-          <div className="bg-white rounded-xl flex flex-col gap-1">
+          <div className="bg-white rounded-xl border border-black/20 shadow-sm flex flex-col gap-1">
             {listElement.map((item) => { 
               return(
               <Link key={item.href} to={item.href} className="flex items-center text-sm justify-between px-4 py-3.5 hover:bg-gray-50 transition-colors">
