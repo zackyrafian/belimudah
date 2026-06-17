37e5707018a8e4c56afc243a86f7dc1be65043fc
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-06-17 21:40:37 +0700

    feat: setup data dynamic at checkout layout

--- src/components/layouts/checkout.layout.jsx
+++ src/components/layouts/checkout.layout.jsx
@@ -1,17 +1,33 @@
 import { Outlet } from "react-router";
 import Card from "../card";
 import MainLayout from "./main.layout";
+import { UserStorage } from "@/services/user.service";
 
-export default function CheckoutLayout ()  { 
+export default function CheckoutLayout() {
+  const { cart } = UserStorage.getCart();
+  
   return (
     <MainLayout>
       <div className="flex gap-4 items-start" >
         <Card className="flex-1">
           <Outlet/>
         </Card>
-        <Card className="w-1/4 flex-col gap-2 flex">
-          <div>Ringkasan Pesan</div>
-          <div className="flex justify-between items-center">
+        <Card className="w-1/4 flex-col gap-2 flex text-sm sticky top-0">
+          <h3 className="text-lg">Ringkasan Pesan</h3>
+          {cart.product.map((item) => (
+            <div className="flex justify-between items-center pb-2">
+              <div className="flex gap-2 items-center">
+                <div className="w-10 h-10 rounded-lg overflow-hidden">
+                  <img src="/headphone.png" alt="" />
+                </div>
+                <span className="text-xs">{item.name}</span>
+              </div>
+              <div>
+                <span>x1</span>
+              </div>
+            </div>
+          ))}
+          {/* <div className="flex justify-between items-center pb-2">
             <div className="flex gap-2 items-center">
               <div className="w-10 h-10 rounded-lg overflow-hidden">
                 <img src="/headphone.png" alt="" />
@@ -21,17 +37,18 @@ export default function CheckoutLayout ()  {
             <div>
               <span>x1</span>
             </div>
-          </div>
-          <div className="flex flex-col gap-1">
+          </div>*/}
+         
+          <div className="flex flex-col gap-2  border-t border-t-black/20 pt-2">
             <div className="flex justify-between">
               <span>Subtotal</span>
               <span>Rp 450.000</span>
             </div>
-            <div className="flex justify-between border-b border-b-black/20">
+            <div className="flex justify-between border-b border-b-black/20 pb-2">
               <span>Ongkir</span>
               <span>Gratis</span>
             </div>
-            <div className="flex justify-between pt-1">
+            <div className="flex justify-between pt-1 pb-4">
               <span>Total</span>
               <span>Rp 450.000</span>
             </div>
