b3fee1090b710d9672406f603f60ea8baaf37736
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-06-18 22:39:17 +0700

    style: setup responsive form selection

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -47,7 +47,7 @@ export default function SignIn () {
           </div>
         </div>
       </div>
-      <div className="flex flex-col w-1/2 gap-4 bg-white lg:p-50 md:p-10 md:pt-50 sm:p-10">
+      <div className="flex flex-col w-1/2 gap-4 bg-white lg:p-40 md:p-10 md:pt-50 sm:p-10">
         <div>
           <h1 className="font-bold text-2xl">Masuk ke Akun</h1>
           <span>Belum punya akun? <Link>Daftar gratis</Link></span>
@@ -57,9 +57,7 @@ export default function SignIn () {
           <div className="rounded-xl border p-4 flex-1 border-black/20 text-center">Google</div>
           <div className="rounded-xl border p-4 flex-1 border-black/20 text-center">Facebook</div>
         </div>
-        
 
-        {/* FORM */}
         <form onSubmit={AuthService.handleLogin} className="flex flex-col gap-4">
           <div className="flex flex-col gap-1">
             <label htmlFor="email">Email</label>
