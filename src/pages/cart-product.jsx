081e3049d1fdc98b61d2df2b1f9c9dcf9959c33b
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-06-19 13:44:40 +0700

    feat: using dynamic product at cart localstorage

--- src/pages/cart-product.jsx
+++ src/pages/cart-product.jsx
@@ -1,55 +1,58 @@
-import { Header } from '../components/header'
-import { Footer } from '../components/footer'
-import { Clock, Heart, Shield, Star, StarIcon, Tag, Trash, Trash2, Zap } from 'lucide-react'
+import { Heart, Shield, Tag, Trash2 } from 'lucide-react'
+import { UserStorage } from '@/services/user.service'
+import { formatIDR } from '@/utils/format';
+import { MainLayout } from '@/components/layouts';
 
 export default function CartPage() {
+  const { cart, total } = UserStorage.getCart();
+  // const [ quantity , setQuantity ] = useState()
   return (
-    <div className="flex flex-col">
-      <Header />
+    <MainLayout className="flex flex-col">
       <div className='w-7xl m-auto flex flex-col gap-4 pt-4'>
-        <div className='text-2xl'>Keranjang Belanja</div>
+        <div className='text-2xl'>Keranjang Belanja { total } Produk</div>
         <div>
-          <div className='flex gap-8'>
+          <div className='flex gap-8 items-start'>
             <div className='flex w-full flex-col gap-4'>
-              <div className='flex w-full bg-white border-black/20 border p-4 gap-4 rounded-xl'>
-                <div className='w-24 h-24'>
-                  <img className='rounded-xl' src="headphone.png" alt="headphone" />
-                </div>
-                <div className='flex flex-col gap-1 flex-1'>
-                  <div className='flex justify-between items-center'>
-                    <span className='text-sm'>Headphone Wireless Premium</span>
-                    <Trash2 size={16} />
+              {cart.product.map((product) => (
+                <div className='shadow-sm flex w-full bg-white border-black/20 border p-4 gap-4 rounded-xl'>
+                  <div className='w-24 h-24'>
+                    <img className='rounded-xl' src={product.images[0]} alt="headphone" />
                   </div>
-
-                  <div>
-                    <span>Hitam</span>
-                  </div>
-
-                  <div className='flex items-center justify-between'>
-                    <div className='border flex px-4 py-1 gap-4 rounded-xl items-center'>
-                      <button className='w-6 text-center'>-</button>
-                      <span className='w-6 text-center'>1</span>
-                      <button className='w-6 text-center'>+</button>
+                  <div className='flex flex-col gap-1 flex-1'>
+                    <div className='flex justify-between items-center'>
+                      <span className='text-sm'>{product.name}</span>
+                      <Trash2 size={16} />
                     </div>
-                    <span>Rp 450.000</span>
-                  </div>
-
-                  <div className='flex items-center gap-1'>
-                    <Heart size={16} />
-                    <span>Simpan ke wishlist</span>
+  
+                    <div>
+                      <span>{product.variant}</span>
+                    </div>
+  
+                    <div className='flex items-center justify-between'>
+                      <div className='border flex px-4 py-1 gap-4 rounded-xl items-center'>
+                        <button className='w-6 text-center'>-</button>
+                        <span className='w-6 text-center'>{product.quantity}</span>
+                        <button onClick={() => } className='w-6 text-center'>+</button>
+                      </div>
+                      <span>{ formatIDR(product.price)}</span>
+                    </div>
+  
+                    <div className='flex items-center gap-1'>
+                      <Heart size={16} />
+                      <span>Simpan ke wishlist</span>
+                    </div>
                   </div>
                 </div>
-              </div>
+              ))}
 
-
-              <div className='flex flex-col w-full border-black/20 border bg-white p-4 gap-4 rounded-xl'>
+              <div className='shadow-sm flex flex-col w-full border-black/20 border bg-white p-4 gap-4 rounded-xl'>
                 <div className='flex gap-2 items-center'>
                   <Tag size={16} />
                   <span>Kode Promo</span>
                 </div>
 
                 <div className='flex gap-2'>
-                  <div className='flex-1 bg-black/15 rounded-xl border border-black/20'>
+                  <div className='flex-1 bg-black/5 rounded-xl border border-black/20'>
                     <input className='w-full px-4 py-3.5 rounded-xl' type="text" placeholder='Masukan kode promo' />
                   </div>
                   <button className='bg-blue-500 rounded-xl px-6 text-white'>Terapkan</button>
@@ -58,16 +61,16 @@ export default function CartPage() {
               </div>
             </div>
 
-            <div className='text-sm flex gap-4 flex-col w-[45%] bg-white p-4 rounded-xl border border-black/20'>
+            <div className='shadow-sm text-sm flex gap-4 flex-col w-[45%] bg-white p-4 rounded-xl border border-black/20'>
               <span className='text-xl font-semibold'>Ringkasan Pesanan</span>
 
               <div className='flex-col flex gap-2'>
                 <div className='flex justify-between'>
-                  <span>Subtotal 1(item)</span>
-                  <span>Rp 450.000</span>
+                  <span>Subtotal {total}(item)</span>
+                  <span>{ 1 }</span>
                 </div>
 
-                <div className='flex justify-between border-b border-b-black/20'>
+                <div className='flex justify-between border-b pb-4 border-b-black/20'>
                   <span>Ongkir Kirim</span>
                   <span>Gratis</span>
                 </div>
@@ -78,10 +81,10 @@ export default function CartPage() {
                 </div>
               </div>
 
-              <div className='bg-orange-500 gap-2 text-sm items-center flex justify-center p-4 text-white rounded-xl'>
+              <button onClick={() => location.href = "/checkout/address"} className='cursor-pointer bg-orange-500 gap-2 text-sm items-center flex justify-center p-4 text-white rounded-xl'>
                 <Shield size={16}/>
                 <span>Checkout Aman</span>
-              </div>
+              </button>
 
               <div className='flex flex-col text-center text-sm'>
                 <span>Pembayaran 100% Aman</span>
@@ -92,7 +95,6 @@ export default function CartPage() {
         </div>
       </div>
 
-      <Footer />
-    </div>
+    </MainLayout>
   )
 }
