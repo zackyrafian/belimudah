b9ccfb42f9da842ea12748147a6476cd340fd0b1
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-06-19 13:42:46 +0700

    feat: confirm & and to order and check checkout localstorage

--- src/pages/checkout/confirm.jsx
+++ src/pages/checkout/confirm.jsx
@@ -1,23 +1,54 @@
+import { UserStorage } from "@/services/user.service";
+import { formatIDR } from "@/utils/format";
 import { Shield } from "lucide-react";
+import { useNavigate } from "react-router";
+import { PackageCheck } from 'lucide-react'
 
-export default function CheckoutConfirmPage () { 
+export default function CheckoutConfirmPage() {
+  const checkout = UserStorage.getUser().checkout;
+  const navigate = useNavigate();
+  const handleForm = () => { 
+    UserStorage.createOrder(checkout);
+    UserStorage.clearCheckout();
+
+    navigate('/checkout/success')
+  }
   return ( 
-    <div className="flex-col flex gap-4">
-      <div>Konfirmasi Pesanan</div>
-
-      <div className="p-4 flex flex-col bg-gray-200 rounded-xl">
-        <span>Alamat Pengiriman</span>
-        <span>Budi Santoso · 0812-3456-7890</span>
-        <span>Jl. Kebon Jeruk No. 15, Jakarta Barat, DKI Jakarta 11530</span>
+    <form onSubmit={handleForm} className="flex-col flex gap-4">
+      <div className="flex items-center gap-2">
+        <PackageCheck />
+        <h3 className="text-xl font-medium">Konfirmasi Pesanan</h3>
+        
       </div>
 
-      <div className="p-4 flex flex-col bg-gray-200 rounded-xl">
+      <div className="p-4 flex flex-col border border-black/20 to-30% rounded-xl">
+        <span>Alamat Pengiriman</span>
+        <span>{checkout.shipping_address
+.recipient_name} · {checkout.shipping_address
+.phone_number}</span>
+        <span>Alamat</span>
+      </div>
+
+      <div className="p-4 flex flex-col border border-black/20 bg-gray-200/30 rounded-xl">
         <span>Metode Pengiriman</span>
         <span>JNE Regular 3-5 hari kerja</span>
       </div>
-      <div className="p-4 flex flex-col gap-2 bg-gray-200 rounded-xl">
+      <div className="p-4 flex flex-col gap-4 border border-black/20 bg-gray-200/30 rounded-xl">
         <span>Produk yang di Pesan</span>
-        <div className="flex justify-between items-center">
+
+        {checkout.cart.map((cart) => (
+          <div className="flex justify-between items-center">
+            <div className="flex gap-4">
+              <div className="w-12 h-12 rounded-xl overflow-hidden"><img src="/headphone.png" alt="headphone" /></div>
+              <div>
+                <div>{cart.name}</div>
+                <div>x{cart.quantity}</div>
+              </div>  
+            </div>
+            <div>Rp 450.000</div>
+          </div>
+        ))}
+        {/* <div className="flex justify-between items-center">
           <div className="flex gap-4">
             <div className="w-12 h-12 rounded-xl overflow-hidden"><img src="/headphone.png" alt="headphone" /></div>
             <div>
@@ -26,7 +57,7 @@ export default function CheckoutConfirmPage () {
             </div>  
           </div>
           <div>Rp 450.000</div>
-        </div>
+        </div>*/}
       </div>
       
       <div className="flex gap-4 items-center border rounded-xl py-2 p-4">
@@ -36,8 +67,8 @@ export default function CheckoutConfirmPage () {
 
       <div className="flex gap-4">
         <button className="border w-30 text-center p-2 rounded-xl">Kembali</button>
-        <button className="flex items-center border flex-1 justify-center p-2 rounded-xl">Bayar Rp 450.000 Sekarang</button>
+        <button className="flex items-center border flex-1 justify-center p-2 rounded-xl" type="submit">Bayar {formatIDR(checkout.total)} Sekarang</button>
       </div>
-    </div>
+    </form>
   )
 }
