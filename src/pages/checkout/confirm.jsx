46678676fab9457cb9ad9c34b4d3ee8e00d85a0b
Zacky Rafian <imftcy@gmail.com>
2026-06-21 15:14:50 +0700

    style: remove backgorund card

--- src/pages/checkout/confirm.jsx
+++ src/pages/checkout/confirm.jsx
@@ -22,18 +22,20 @@ export default function CheckoutConfirmPage() {
       </div>
 
       <div className="p-4 flex flex-col border border-black/20 to-30% rounded-xl">
-        <span>Alamat Pengiriman</span>
-        <span>{checkout.shipping_address
-.recipient_name} · {checkout.shipping_address
-.phone_number}</span>
-        <span>Alamat</span>
+        <span className="pb-2">Alamat Pengiriman</span>
+        <h3 className="">{checkout.shipping_address.recipient_name} · <span className="text-gray-500 text-sm">{checkout.shipping_address.phone_number}</span></h3>
+        {/* <span></span> */}
+        <div className="flex flex-col gap-1">
+          <span className="text-sm">{checkout.shipping_address.recipient_address_full}</span>
+          <span className="text-xs">{checkout.shipping_address.recipient_city}, {checkout.shipping_address.recipient_province} - {checkout.shipping_address.zip_code}</span>
+        </div>
       </div>
 
-      <div className="p-4 flex flex-col border border-black/20 bg-gray-200/30 rounded-xl">
+      <div className="p-4 flex flex-col border border-black/20 rounded-xl">
         <span>Metode Pengiriman</span>
         <span>JNE Regular 3-5 hari kerja</span>
       </div>
-      <div className="p-4 flex flex-col gap-4 border border-black/20 bg-gray-200/30 rounded-xl">
+      <div className="p-4 flex flex-col gap-4 border border-black/20 rounded-xl">
         <span>Produk yang di Pesan</span>
 
         {checkout.cart.map((cart) => (
@@ -45,7 +47,7 @@ export default function CheckoutConfirmPage() {
                 <div>x{cart.quantity}</div>
               </div>  
             </div>
-            <div>Rp 450.000</div>
+            <div>{formatIDR(cart.price * cart.quantity)}</div>
           </div>
         ))}
         {/* <div className="flex justify-between items-center">
@@ -60,14 +62,14 @@ export default function CheckoutConfirmPage() {
         </div>*/}
       </div>
       
-      <div className="flex gap-4 items-center border rounded-xl py-2 p-4">
+      <div className="flex gap-4 items-center border border-blue-200 bg-blue-50 text-black rounded-xl py-2 p-4">
         <Shield size={28}/>
         <span className="text-sm">Dengan menekan "Bayar Sekarang", kamu menyetujui Syarat & Ketentuan kami. Pembayaran baru akan diproses setelah kamu mengkonfirmasi di langkah ini.</span>
       </div>
 
       <div className="flex gap-4">
-        <button className="border w-30 text-center p-2 rounded-xl">Kembali</button>
-        <button className="flex items-center border flex-1 justify-center p-2 rounded-xl" type="submit">Bayar {formatIDR(checkout.total)} Sekarang</button>
+        <button className="cursor-pointer border w-30 text-center border-black/20 p-2 rounded-xl">Kembali</button>
+        <button className="cursor-pointer flex items-center border border-black/20 flex-1 justify-center p-2 rounded-xl" type="submit">Bayar {formatIDR(checkout.total)} Sekarang</button>
       </div>
     </form>
   )
