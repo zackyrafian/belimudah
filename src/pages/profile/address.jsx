590ceba522637ef645e9614f4ebffa154470f488
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-06-22 13:35:54 +0700

    feat: add dynamic priceTotal

--- src/pages/profile/address.jsx
+++ src/pages/profile/address.jsx
@@ -2,11 +2,30 @@ import { Edit, MapPin, PlusIcon, Trash2, X } from "lucide-react";
 import { Card } from "../../components";
 import { UserStorage } from "@/services/user.service";
 import { useState } from "react";
+import { useAlert } from "@/hooks/useAlert";
+import Alert from "@/components/ui/alert";
 export default function ProfileAddress() { 
+  const { alert, showSuccess, clearAlert } = useAlert();
   const [dialog, setDialog] = useState(false);
   const user = UserStorage.getUser();
+
+  const handleForm = (e) => { 
+    const data = Object.fromEntries(new FormData(e.currentTarget))
+    UserStorage.updateShippingAddress(data);
+    showSuccess("Berhasil menambahkan Alamat")
+  }
   return ( 
     <div className="flex flex-col gap-2">
+      <div>
+        {alert && (
+          <Alert
+            title={"Profile Address"}
+            key={new Date}
+            type={alert.type}
+            message={alert.message}
+            onClose={() => clearAlert()}
+          />
+      )}
       {dialog && (
         <div className="fixed inset-0 bg-black/50 items-center justify-center z-50 flex">
           <Card className="p-6 flex flex-col min-w-1/2 gap-4">
@@ -19,7 +38,7 @@ export default function ProfileAddress() {
                 <X/>
               </button>
             </div>
-            <form className="flex flex-col gap-6">
+            <form onSubmit={handleForm} className="flex flex-col gap-6">
               <div className="flex gap-4">
                 <div className="flex flex-col w-1/2">
                   <label htmlFor="">Nama Pengirim *</label>
@@ -74,32 +93,34 @@ export default function ProfileAddress() {
         </button>
       </div>
 
-      {user?.shipping_address?.map((address) => (
-        <Card className="flex flex-col gap-4">
-          <div className="flex justify-between items-center">
-            <div className="flex gap-2 font-bold">
-              <span>Rumah Utama</span>
-              <div className="rounded-full text-xs flex items-center bg-blue-500 text-white px-4">Utama</div>
-            </div>
-            <div className="flex gap-4">
-              <Edit size={15}/>
-              <Trash2 size={15}/>
-            </div>
-          </div>
-  
-          <div className="flex flex-col gap-1">
-            <div className="flex items-center gap-3">
-              <h3 className="font-semibold"> Budi Santoso</h3>
-              <span className="text-sm text-gray-500">0812-3456-7890</span>
-            </div>
-            <div className="text-sm text-gray-700 leading-relaxed">
-              Jl. Kebon Jeruk No. 15, RT.003/RW.002
-              <br />
-              Jakarta Barat, DKI Jakarta 11530
-            </div>
-          </div>
-        </Card>
-      ))}
+        <div className="flex flex-col gap-4 pt-4">
+          {user?.shipping_address?.map((address) => (
+            <Card className="flex flex-col gap-4">
+              <div className="flex justify-between items-center">
+                <div className="flex gap-2 font-bold">
+                  <span>Rumah Utama</span>
+                  <div className="rounded-full text-xs flex items-center bg-blue-500 text-white px-4">Utama</div>
+                </div>
+                <div className="flex gap-4">
+                  <Edit size={15}/>
+                  <Trash2 size={15}/>
+                </div>
+              </div>
+      
+              <div className="flex flex-col gap-1">
+                <div className="flex items-center gap-3">
+                  <h3 className="font-semibold">{address.recipient_name}</h3>
+                  <span className="text-sm text-gray-500">{address.phone_number}</span>
+                </div>
+                <div className="text-sm text-gray-700 leading-relaxed">
+                  {address.recipient_address_full}
+                </div>
+                <span className="text-xs text-gray-500">{address.recipient_city}, {address.recipient_province} - {address.zip_code}</span>
+              </div>
+            </Card>
+          ))}
+        </div>
+      
       {/* <Card className="flex flex-col gap-4">
         <div className="flex justify-between items-center">
           <div className="flex gap-2 font-bold">
@@ -139,6 +160,7 @@ export default function ProfileAddress() {
           <span>Jadikan Alamat Utama</span>
         </div>
       </Card>*/}
+      </div>
     </div>
   )
 }
