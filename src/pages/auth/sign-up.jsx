170bc480fbe017323885c4790a7a8336a57bc582
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-06-23 17:24:58 +0700

    refactor: using hooks form

--- src/pages/auth/sign-up.jsx
+++ src/pages/auth/sign-up.jsx
@@ -1,33 +1,23 @@
 import { Mail, Lock, User } from "lucide-react"
-import { Link, useNavigate } from "react-router"
+import { Link } from "react-router"
 import { AuthService } from "@/services/auth.service"
 import Alert from "@/components/ui/alert"
 import { useAlert } from "@/hooks/useAlert"
+import { FaGoogle, FaFacebook } from "react-icons/fa"
+import { useForm } from 'react-hook-form'
 
 export default function SignUpPage() {
   const { alert, showError, showSuccess, clearAlert } = useAlert();
-  const navigate = useNavigate();
-    const handleSubmit = (e) => {
-      e.preventDefault();
-  
-      try {
-        const form = new FormData(e.target);
-        const data = Object.fromEntries(form.entries());
-        AuthService.register(data);
-        showSuccess("Account created successfully. Your registration is complete and your profile has been set up.")
-        // setAlert({
-        //   type: "success",
-        //   message:
-        //     "Account created successfully. Your registration is complete and your profile has been set up.",
-        // });
-        setTimeout(() => { 
-          navigate('/sign-in')
-        }, [1000])
-  
-      } catch (err) {
-        showError(err.message)
-      }
-    };
+  const { register, handleSubmit } = useForm(); 
+
+  const onSubmit = (data) => { 
+    try { 
+      AuthService.register(data); 
+      showSuccess("You have successfully signed in.")
+    } catch (err) { 
+      showError(err.message);
+    }
+  }
   
   return ( 
     <div className="flex min-h-screen">
@@ -90,18 +80,24 @@ export default function SignUpPage() {
           <span>Belum punya akun? <Link>Daftar gratis</Link></span>
         </div>
 
-        <div className="flex gap-4">
-          <div className="rounded-xl border p-4 flex-1 border-black/20 text-center">Google</div>
-          <div className="rounded-xl border p-4 flex-1 border-black/20 text-center">Facebook</div>
+        <div className="flex gap-2">
+          <div className="flex gap-4 text-center rounded-xl border px-4 py-2 flex-1 border-black/20 items-center justify-center">
+            <FaGoogle/>
+            <span>Google</span>
+          </div>
+          <div className="flex min-w-1/2 gap-4 items-center justify-center rounded-xl border py-2 flex-1 border-black/20 text-center">
+           <FaFacebook/>
+            <span>Facebook</span>
+          </div>
         </div>
 
         {/* FORM */}
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
           <div className="flex flex-col gap-1">
             <label htmlFor="">Nama Lengkap</label>
             <div className="flex p-4 border border-black/20 rounded-xl items-center gap-2">
               <User className="text-gray-500" size={20}/>
-              <input name="fullname" className="w-full h-full outline-none" type="text" placeholder="Nama Lengkap kamu"/>
+              <input {...register('fullname')} className="w-full h-full outline-none" type="text" placeholder="Nama Lengkap kamu"/>
             </div>
           </div>
 
@@ -109,26 +105,25 @@ export default function SignUpPage() {
             <label htmlFor="">Email</label>
             <div className="flex p-4 border border-black/20 rounded-xl items-center gap-2">
               <Mail className="text-gray-500" size={20}/>
-              <input name="email" className="w-full h-full outline-none" type="text" placeholder="@email.contoh.com"/>
+              <input {...register('email')} className="w-full h-full outline-none" type="text" placeholder="@email.contoh.com"/>
             </div>
           </div>
 
           <div className="flex flex-col gap-1">
             <label htmlFor="">Kata Sandi</label>
-            <div className="flex p-4 border border-black/20 rounded-xl items-center gap-2">
+            <div className="flex px-4 py-3 border border-black/20 rounded-xl items-center gap-2">
               <Lock className="text-gray-500" size={20}/>
-              <input name="password" className="w-full h-full outline-none" type="password" placeholder="Password"/>
+              <input {...register('password')} className="w-full h-full outline-none" type="password" placeholder="Password"/>
             </div>
           </div>
 
-          <div className="flex flex-col gap-1">
+          <div className="flex flex-col gap-1 text-sm">
             <label htmlFor="">Kata Sandi</label>
-            <div className="flex p-4 border border-black/20 rounded-xl items-center gap-2">
+            <div className="flex px-4 py-3 border border-black/20 rounded-xl items-center gap-2">
               <Lock className="text-gray-500" size={20}/>
-              <input name="confirmPassword" className="w-full h-full outline-none" type="password" placeholder="Confirm Password"/>
+              <input {...register('confirmPassword')} className="w-full h-full outline-none" type="password" placeholder="Confirm Password"/>
             </div>
           </div>
-          
           <div className="flex gap-2">
             <input type="checkbox" />
             <span>Ingat saya selama 30 hari</span>
@@ -136,7 +131,7 @@ export default function SignUpPage() {
           <button className="text-center bg-blue-500 w-full p-4 rounded-xl text-white" type="submit">Daftar Sekarang</button>
         </form>
 
-        <div className="text-center">
+        <div className="text-center text-xs">
           <div>Login aman dengan enskripsi SSL 256-bit</div>
           <div>Dengan masuk, kamu menyetujui Syarat & Ketentuan dan Kebijakan Privasi kami.</div>
         </div>
