ed3ed7c7a77363bd13b31d20d7d762b1dddaf7a0
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-06-26 11:35:51 +0700

    chore: remove unused service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,133 +1,22 @@
-const UserStorage = { 
-  getAccounts() { 
+
+const UserStorage = {
+  getAccounts() {
     return JSON.parse(localStorage.getItem("account") || "[]")
   },
 
-  getUser() { 
-    return JSON.parse(localStorage.getItem("user")); 
-  }, 
-
-  updateUser(updateData) { 
-    const user = this.getUser();
-    if (!user) { 
-      return; 
-    }
-    const accounts = this.getAccounts()
-    const index = accounts.findIndex((account) => account.email === user.email); 
-    if (index === -1) return;
-    
-    const updatedUser = {
-      ...accounts[index],
-      ...updateData
-    };
-  
-    accounts[index] = updatedUser;
-    localStorage.setItem("account", JSON.stringify(accounts));
-    localStorage.setItem("user", JSON.stringify(updatedUser));
-
-    return updatedUser;
-  },
-
-  addCart(product) { 
-    const user = this.getUser(); 
-    if (!user) { 
-      return; 
-    }
-    const cart = [...(user.cart || [])];
-    cart.push(product)
-    return this.updateUser({cart})
-  },
-
-  getCart() { 
-    const user = this.getUser(); 
-    if (!user) { 
-      return window.location.href = '/';
-    }
-    
-    return {
-      cart: {
-        product: [...user.cart], 
-      },
-      total: user.cart.length
-    };
-  },
-
-  removeFromCart(idx) {
-    const user = this.getUser();
+  syncUser(user) {
     if (!user) return;
-    const cart = [...(user.cart || [])];
-    cart.splice(idx, 1);
 
-    return this.updateUser({ cart });
-  },
+    const accounts = this.getAccounts();
+    const index = accounts.findIndex(acc => acc.email === user.email);
 
-  updateShippingAddress(address) { 
-    const user = this.getUser();
-    const shipping = user.shipping_address || [];
-    const updated = [...shipping, address];
-    
-    return this.updateUser({
-      shipping_address: updated
-    });
-  },
-
-  setSelectedShippingAddress(address) {
-    return this.setCheckout({
-      shipping_address: address
-    });
-  },
-
-  setSelectedPaymentMethod(method) {
-    return this.setCheckout({
-      payment_method: method,
-    });
-  },
-
-  createCheckout(data) {
-    return this.updateUser({
-      checkout: data
-    });
-  },
-
-  setCheckout(data) { 
-    const user = this.getUser(); 
-    return this.updateUser({
-      checkout: {
-        ...(user.checkout || {}),
-        ...data,
+    if (index !== -1) {
+      accounts[index] = {
+        ...accounts[index],
+        ...user,
+        password: accounts[index].password
       }
-    });
-  },
-  clearCheckout() {
-    return this.updateUser({
-      checkout: {},
-      cart: []
-    });
-  },
-
-  createOrder(data) { 
-    const user = this.getUser(); 
-    if (!user) { 
-      return; 
-    }
-    const order = [...(user.order || [])];
-    order.push(data)
-    return this.updateUser({order})
-  },
-
-  syncUser() { 
-    const user = this.getUser(); 
-    if(!user) return; 
-
-    const accounts = this.getAccounts(); 
-    const index = accounts.findIndex(acc => acc.email === user.email); 
-
-    if (index !== -1) { 
-      accounts[index] = {...accounts[index], ...user}; 
-      localStorage.setItem("account", JSON.stringify(accounts)); 
-
-      const mergeUser = {...user, ...accounts[index]}; 
-      localStorage.setItem("user", JSON.stringify(mergeUser));
+      localStorage.setItem("account", JSON.stringify(accounts));
     }
   }
 }
