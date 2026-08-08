212d670b87803ba09d633d6d7f221f8d86aeffb8
Zacky Rafian <imftcy@gmail.com>
2026-08-08 14:37:17 +0700

    feat: add auth thunks for login and user info

---
+++ src/features/auth/authThunks.js
@@ -0,0 +1,38 @@
+import { AuthService } from "@/services/auth.service"
+import { UserService } from "@/services/user.service"
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const loginAsync = createAsyncThunk(
+  `auth/login`, 
+  async(credentials, { rejectWithValue }) => { 
+    try {
+      const data = await AuthService.login(credentials);
+      return data;
+    } catch(error) { 
+      return rejectWithValue(error.message);
+    }
+  }
+)
+
+export const userInfoAsync = createAsyncThunk(
+  `auth/fetchUserInfo`,
+  async (_, { rejectWithValue, getState }) => {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        throw new Error("No token found");
+      }
+      const userInfo = await UserService.getUserInfo(token);
+      return userInfo;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+)
+
+const authThunks = { 
+  loginAsync, 
+  userInfoAsync,
+}
+
+export default authThunks;
